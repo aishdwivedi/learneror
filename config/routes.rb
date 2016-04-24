c921bd6d431f376c42ec6d2b24ee34c0bb45aa26
Aishwarya Dwivedi <aish.dwivedi@gmail.com>
2016-04-24 15:02:50 +0000

    more layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  #devise_for :users
  root to: 'pages#index'
 
   get 'pages/contact'
