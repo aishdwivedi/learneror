423e59afaf8c9298eb972baddec7b18ee487d644
Aishwarya Dwivedi <aish.dwivedi@gmail.com>
2016-04-25 03:43:37 +0000

    articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   #devise_for :users
+  resources :articles
  root to: 'pages#index'
 
   get 'pages/contact'
