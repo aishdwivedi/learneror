1b98b16259336e037ee89e27cbe64766ad96b73f
Aishwarya Dwivedi <aish.dwivedi@gmail.com>
2016-05-01 05:58:14 +0000

    fixing conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  #devise_for :users
-  resources :articles
-=======
   devise_for :users , :controllers => { registations: 'registations' }
   resources :articles
   resources :contacts
->>>>>>> style
  root to: 'pages#index'
 
   get 'pages/contact'
