6d91eee453d746d7176396a44066f911cdcd901d
Aishwarya Dwivedi <aish.dwivedi@gmail.com>
2016-05-01 02:05:18 +0000

    mailer lesson

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @contact = Contact.new
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , :controllers => { registations: 'registations' }
+  resources :articles
+  resources :contacts
  root to: 'pages#index'
 
   get 'pages/contact'
 
   get 'pages/about'
 
-  
+  resources :contacts
 end
